7d688a7beef623c2a23d067da7d47fd037e8b00f
Simon Emmanuel <simonemmanuel743@gmail.com>
2023-06-16 08:09:49 +0100

    student profile completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* =====Nav===== */
 const button = document.getElementById('btn-click');
 const menu = document.getElementById('menu-li');
 
@@ -7,4 +8,31 @@ function showMenu() {
     button.classList.toggle('active');
     menu.classList.toggle('active');
 }
+/* =====Nav Ends===== */
+
+
+/* =====Student Profile===== */
+const profileButton = document.querySelectorAll('.student-name');
+const studentProfile = document.querySelectorAll('.student-profile-main-container');
+
+for (let index = 0; index < studentProfile.length; index++) {
+    studentProfile[index].style.display = "none";   
+
+    for (let index = 0; index < profileButton.length; index++) {
+    
+        profileButton[index].addEventListener('click', function () {
+
+            switch (studentProfile[index].style.display) {
+                case 'none':
+                    studentProfile[index].style.display = "block";
+                    break;
+            
+                default:
+                    studentProfile[index].style.display = "none";
+                    break;
+            }
+        });
+    };
+
+};
 
